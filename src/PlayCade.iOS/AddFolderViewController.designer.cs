e3d64d6cfd19201b10b4a03b71b4d4447a28d57a
Glenn Hevey <glenn.hevey@gmail.com>
2020-05-18 22:17:22 +1000

    Added Initial UI Structure

---
+++ src/PlayCade.iOS/AddFolderViewController.designer.cs
@@ -0,0 +1,26 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Rider IDE
+//   to store outlets and actions made in Xcode.
+// If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace PlayCade.iOS
+{
+	[Register ("AddFolderViewController")]
+	partial class AddFolderViewController
+	{
+		[Action ("CancelButton:")]
+		partial void CancelButton (UIKit.UIBarButtonItem sender);
+
+		[Action ("SaveButton:")]
+		partial void SaveButton (UIKit.UIBarButtonItem sender);
+
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
